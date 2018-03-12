ce0b64264db32504142a921a708f9eec6e93394b
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-03-12 16:15:30 -0300

    state_to_ansible_inventory: improve generated ansible inventory

--- terraform/scripts/state_to_ansible_inventory.py
+++ terraform/scripts/state_to_ansible_inventory.py
@@ -4,6 +4,8 @@ import json
 import os
 import sys
 
+environment = os.getenv('ENV', 'staging')
+
 state = json.loads(sys.stdin.read())
 
 inventory = {
@@ -12,25 +14,12 @@ inventory = {
 }
 database = {}
 
-environment = None
 for module in state['modules']:
     for resource_name, resource in module['resources'].items():
         if "aws_instance" in resource_name:
             ip = resource['primary']['attributes']['public_ip']
             name = resource['primary']['attributes']['tags.Name']
-            if environment:
-                assert environment in name, "Unknown environment {}".format(name)
-            else:
-                # Discover environment
-                # This may be too clever. may be better to use a cli arg
-                if 'staging' in name:
-                    environment = "staging"
-                elif 'production' in name:
-                    environment = "production"
-                else:
-                    assert false, "Unknown environment {}".format(name)
-
-
+            hostname = resource['primary']['attributes']['private_dns']
             host_type = None
             if 'www' in name:
                 host_type = "webserver"
@@ -42,17 +31,29 @@ for module in state['modules']:
             inventory[host_type].append({
                 "name": name,
                 "ip": ip,
+                "hostname": hostname,
             })
         if "aws_db_instance" in resource_name:
             database['name'] = resource['primary']['attributes']['address']
 
-print("[{}]".format(environment))
-print("[{}:{}]".format(environment, "webserver"))
+print("[webservers]")
+master_hostname = None
 for host in inventory["webserver"]:
-    print('{} ansible_host={}'.format(host['name'], host['ip']))
-print("[{}:{}]".format(environment, "worker"))
+    if "www-0" in host['name']:
+        master_hostname = host['hostname'].split('.')[0] # get only local part
+        master = ' master_node=1'
+    else:
+        master = ''
+    print('{} ansible_host={} ansible_user=ubuntu{}'.format(host['name'], host['ip'], master))
+print("[workers]")
 for host in inventory["worker"]:
-    print('{} ansible_host={}'.format(host['name'], host['ip']))
-print("[{}:{}]".format(environment, "database"))
-print(database['name'])
+    print('{} ansible_host={} ansible_user=ubuntu'.format(host['name'], host['ip']))
+print("[{}:children]".format(environment))
+print("webservers")
+print("workers")
 
+print("[{}:vars]".format(environment))
+print('master_node=0')
+print('master_hostname={}'.format(master_hostname))
+print("database_hostname={}".format(database['name']))
+print('ansible_ssh_common_args="-o StrictHostKeyChecking=no"')
