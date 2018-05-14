0c9f52be9e96c47509e5e10b923ca903f0a7c172
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-05-14 17:19:47 -0300

    Fix Linaro LDAP authentication

--- ansible/roles/squad/templates/linaro_ldap_backends.py
+++ ansible/roles/squad/templates/linaro_ldap_backends.py
@@ -11,24 +11,3 @@ class LDAPUsernameBackend(LDAPBackend):
 
 class LDAPEmailBackend(LDAPBackend):
     settings_prefix = "AUTH_LDAP_E_"
-
-    def get_or_create_user(self, email, ldap_user):
-        """
-        Use the Posixuser uid field as username instead of form value (email).
-
-        This must return a (User, created) 2-tuple for the given LDAP user.
-        username is the Django-friendly username of the user. ldap_user.dn is
-        the user's DN and ldap_user.attrs contains all of their LDAP attributes.
-        """
-        model = self.get_user_model()
-        username_field = getattr(model, 'USERNAME_FIELD', 'username')
-
-        kwargs = {
-            username_field + '__iexact': ldap_user.attrs['uid'][0],
-            'defaults': {
-                username_field: ldap_user.attrs['uid'][0].lower(),
-                'email': email
-            }
-        }
-
-        return model.objects.get_or_create(**kwargs)
